af4ba055768db5f85276d6199623a30796f619fa
Thavamani Murugan <thavamani1304@gmail.com>
2021-07-28 22:37:24 +0530

    "Updated problems"

---
+++ src/main/java/dailychallenge/CW08_FindPeakElementUsingBS.java
@@ -0,0 +1,17 @@
+package dailychallenge;
+
+import org.junit.Test;
+
+public class CW08_FindPeakElementUsingBS {
+
+	@Test
+	public void testcase1() {
+		int[] intarr = { 1, 2, 3, 1 };
+		findPeakElement(intarr);
+	}
+
+	private void findPeakElement(int[] intarr) {
+
+	}
+
+}
